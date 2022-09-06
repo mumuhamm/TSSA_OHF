82f4217ed11df797e55a3b32986903097f2e495d
Muhammad Alibordi <alibordi@rcas2076.rcf.bnl.gov>
2022-09-05 21:06:14 -0400

    small fix

--- submit.sh
+++ submit.sh
@@ -2,14 +2,19 @@
 count=0
 for file in /gpfs/mnt/gpfs02/phenix/spin/spin1/phnxsp01/sanghwa/taxi/Run15pp200CAMUP108/18416/data/4*.root ; do
     echo $count $file
+    run=${file%.*}
+    run=${run##*/}
+    echo $run
+    echo $file
     count=$((count+1))
+    tag=$count'_'$run
     purefile='basename $file'
-    stem='basename $purefile.root'
+    stem='total_job'
     condor_submit \
     -a "output = $stem.out" \
     -a "error = $stem.error" \
     -a "Log = $stem.log" \
-    -a "Arguments = $file $count" \
+    -a "Arguments = $file $tag" \
     condor_v2
 done
 
