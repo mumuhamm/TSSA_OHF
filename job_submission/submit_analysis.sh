f484e083bba61ae7dbcb0553033e80ff69b1d10f
Muhammad Alibordi <alibordi@rcas2076.rcf.bnl.gov>
2022-09-20 01:39:26 -0400

    added a few variable

--- job_submission/submit_analysis.sh
+++ job_submission/submit_analysis.sh
@@ -1,15 +1,19 @@
 #! /bin/sh
 count=0
-for file in /gpfs/mnt/gpfs02/phenix/spin/spin1/phnxsp01/alibordi/HF_Analysis/muon_output/*.root ; do
+for file in /gpfs/mnt/gpfs02/phenix/spin/spin1/phnxsp01/alibordi/HF_Analysis/muon_output/analysis_preliminary_*.root ; do
     echo $count $file
     count=$((count+1))
+    name=${file%.*}
+    name=${name##*/}
+    run=$(echo $name| cut -d'_' -f 4)
+    echo $run
     purefile='basename $file'
     stem='analysis_job'
     condor_submit \
     -a "output = $stem.out" \
     -a "error = $stem.error" \
     -a "Log = $stem.log" \
-    -a "Arguments = $file $count" \
+    -a "Arguments = $file $run" \
     condor_v2_analysis
 done
 
